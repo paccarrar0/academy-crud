f2806b8a44a3b9edc4aee0895c80ec40c2157fb0
Pedro Carraro <pedrocarraro88@gmail.com>
2024-06-05 15:45:29 -0300

    compose fix

--- html/config.php
+++ html/config.php
@@ -1,7 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', '172.18.0.2');
+define('DB_SERVER', 'mysql');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '123456');
 define('DB_NAME', 'academydb');
