eb3d045d3cfebf52e8143247c7323fffd8771783
Pedro Carraro <pedrocarraro88@gmail.com>
2024-06-05 16:52:17 -0300

    read update

--- html/read.php
+++ html/read.php
@@ -5,7 +5,7 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
     require_once "config.php";
     
     // Prepare a select statement
-    $sql = "SELECT * FROM employees WHERE id = ?";
+    $sql = "SELECT * FROM tasks WHERE id = ?";
     
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
@@ -24,9 +24,9 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
                 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
                 
                 // Retrieve individual field value
-                $name = $row["name"];
-                $address = $row["address"];
-                $salary = $row["salary"];
+                $name = $row["task"];
+                $address = $row["data"];
+                $salary = $row["priority"];
             } else{
                 // URL doesn't contain valid id parameter. Redirect to error page
                 header("location: error.php");
@@ -34,7 +34,7 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
             }
             
         } else{
-            echo "Oops! Something went wrong. Please try again later.";
+            echo "Algo deu errado, tente novamente mais tarde.";
         }
     }
      
@@ -54,7 +54,7 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>View Record</title>
+    <title>AcademyTask</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         .wrapper{
@@ -68,20 +68,20 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h1 class="mt-5 mb-3">View Record</h1>
+                    <h1 class="mt-5 mb-3">Visualizar tarefa</h1>
                     <div class="form-group">
                         <label>Name</label>
-                        <p><b><?php echo $row["name"]; ?></b></p>
+                        <p><b><?php echo $row["task"]; ?></b></p>
                     </div>
                     <div class="form-group">
                         <label>Address</label>
-                        <p><b><?php echo $row["address"]; ?></b></p>
+                        <p><b><?php echo $row["data"]; ?></b></p>
                     </div>
                     <div class="form-group">
                         <label>Salary</label>
-                        <p><b><?php echo $row["salary"]; ?></b></p>
+                        <p><b><?php echo $row["priority"]; ?></b></p>
                     </div>
-                    <p><a href="index.php" class="btn btn-primary">Back</a></p>
+                    <p><a href="index.php" class="btn btn-primary">Voltar</a></p>
                 </div>
             </div>        
         </div>
