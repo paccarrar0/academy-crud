30447372f3c06d52eda33e4331d3ca66c924c9e2
Pedro Carraro <pedrocarraro88@gmail.com>
2024-06-05 19:00:30 -0300

    .

--- html/index.php
+++ html/index.php
@@ -16,16 +16,27 @@
         table tr td:last-child{
             width: 120px;
         }
+        .border {
+                border: 1px solid #0A8967 !important;
+                border-radius: 10px;
+        }
+        .btn1{
+                background-color: #0A8967;
+                color: white;
+        }
     </style>
 </head>
 <body>
     <div class="wrapper">
+        <div class="d-flex justify-content-center mt-4 border">
+                <img src="https://res.cloudinary.com/dnbbxja52/image/upload/v1717622700/academytask-devops/logo.svg" alt="logo">
+        </div>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
                         <h2 class="pull-left">Tarefas</h2>
-                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Nova Tarefa</a>
+                        <a href="create.php" class="btn btn1 pull-right"><i class="fa fa-plus"></i> Nova Tarefa</a>
                     </div>
                     <?php
                     
@@ -56,9 +67,9 @@
                                         echo "<td>" . $formattedDate . "</td>";
                                         echo "<td>" . $row['priority'] . "</td>";
                                         echo "<td>";
-                                            echo '<a href="read.php?id='. $row['id'] .'" class="mr-3"><span class="fa fa-eye"></span></a>';
-                                            echo '<a href="update.php?id='. $row['id'] .'" class="mr-3"><span class="fa fa-pencil"></span></a>';
-                                            echo '<a href="delete.php?id='. $row['id'] .'" "><span class="fa fa-trash"></span></a>';
+                                            echo '<a href="read.php?id='. $row['id'] .'" class="mr-3"><span class="fa fa-eye text-black"></span></a>';
+                                            echo '<a href="update.php?id='. $row['id'] .'" class="mr-3"><span class="fa fa-pencil text-black"></span></a>';
+                                            echo '<a href="delete.php?id='. $row['id'] .'" "><span class="fa fa-trash text-black"></span></a>';
                                         echo "</td>";
                                     echo "</tr>";
                                 }
